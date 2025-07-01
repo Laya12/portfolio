0b942ed1b18740b40c6af286aa253d61d3e96fb8
layaaluri12@gmail.com <layaaluri12@gmail.com>
2025-07-01 18:29:14 -0400

    Changed profile picture

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -5,7 +5,7 @@ const MobileMenu = ({ menuOpen }) => {
   if (!menuOpen) return null;
 
   return (
-    <div className='absolute fixed border-1 z-50 top-full right-0 cursor-pointer w-40  bg-white text-white shadow-lg rounded-lg  md:hidden'>
+    <div className='fixed border-1 z-50 top-full right-0 cursor-pointer w-40  bg-white text-white shadow-lg rounded-lg  md:hidden'>
       <Link
         to="/skills"
         className="block px-4  py-2 text-sm text-gray-800 hover:bg-gray-100 rounded-md transition-colors"
